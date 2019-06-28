a7f0da09ab66ec482d2eeee88b5eefd050c8e311
Samuel Rizzo <rizzolabs@gmail.com>
2019-06-28 13:32:39 -0300

    Add --refresh-rate

--- jupyterlive/jupyterlive.py
+++ jupyterlive/jupyterlive.py
@@ -1,4 +1,5 @@
 import ipywidgets as widgets
+from IPython.core.magic_arguments import magic_arguments, argument, parse_argstring
 from IPython.core.magic import line_magic, cell_magic, line_cell_magic, Magics, magics_class
 from jupyter_interval_widget import Interval
 from IPython.display import display, clear_output
@@ -6,7 +7,16 @@ from IPython.display import display, clear_output
 @magics_class
 class JupyterLiveMagics(Magics):
     @cell_magic
+    @magic_arguments()
+    @argument("-r", "--refresh-rate",
+        type=int,
+        default=1000,
+        help="Refresh rate in milliseconds"
+    )
     def live(self, line='', cell=None):
+        args = parse_argstring(self.live, line)
+        refresh_rate = args.refresh_rate
+
         outer = self
         output = widgets.Output()
         def update_output(self):
@@ -14,7 +24,7 @@ class JupyterLiveMagics(Magics):
                 clear_output(wait=True)
                 outer.shell.run_cell(cell, store_history=False)
 
-        interval = Interval(value=500)
+        interval = Interval(value=refresh_rate)
         interval.on_tick(update_output)
 
         display(widgets.HBox(children=(output, interval)))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="jupyter-live",
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/srizzo/jupyter-live',
     author="Samuel Rizzo",
     author_email='rizzolabs@gmail.com',
